d77668c14d5ce0e921db9d9b0a4c49d001f860a7
Apple <ayushm4569@gmail.com>
2025-03-05 21:39:58 +0530

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,14 @@ const app = express();
 const port = process.env.PORT || 8080;
 // middlewares
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["https://edunext.run.place"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
