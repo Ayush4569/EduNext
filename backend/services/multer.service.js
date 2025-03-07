af804e2da358ac315acf040f54a65ac16e6792d4
Apple <ayushm4569@gmail.com>
2025-03-07 13:00:33 +0530

    fixed multer config

--- backend/services/multer.service.js
+++ backend/services/multer.service.js
@@ -1,8 +1,12 @@
 import multer from "multer";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve("./public/uploads"));
+    cb(null,path.join(__dirname, "../public/uploads"));
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
